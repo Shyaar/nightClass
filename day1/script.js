1f6e8581a028fd62398ffd5f470a78ff7b3b5274
Shyaar Sati Fwatshak <shyaarsati@gmial.com>
2025-03-12 02:09:33 +0100

    update to code

---
+++ day1/script.js
@@ -0,0 +1,21 @@
+alert("error")
+
+let btn = document.getElementById("Submit")
+let firstName = document.getElementById("firstName")
+let lastName = document.getElementById("lastName")
+let email = document.getElementById("email")
+let Address = document.getElementById("Address")
+let age = document.getElementById("age")
+
+btn.addEventListener('click'(()=>{
+    alert("error")
+//  e.preventDefault()
+
+ if(firstName==""){
+    let error = document.createElement(<p>field is required</p>)
+    alert(error)
+
+    // firstName.appendChild(before, error)
+
+ }
+}))
